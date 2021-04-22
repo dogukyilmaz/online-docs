577f73f6cff0d32697ddc892179d3701f6e1d314
Kaan Yilmaz <douscriptist@gmail.com>
2021-04-22 03:08:32 +0300

    editor toolbar

--- client/src/components/TextEditor.tsx
+++ client/src/components/TextEditor.tsx
@@ -4,13 +4,28 @@ import "quill/dist/quill.snow.css";
 
 interface Props {}
 
+const TOOLBAR = [
+  [{ font: [] }],
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  ["bold", "italic", "underline" /*, "strike"*/],
+  [{ list: "ordered" }, { list: "bullet" }],
+  // [{ indent: "-1" }, { indent: "+1" }],
+  [{ align: [] }],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  ["link"],
+  ["image", "blockquote", "code-block"],
+  // [{ direction: "rtl" }],
+  ["clean"],
+];
+
 const TextEditor = (props: Props) => {
   const editorRef = useCallback((wrapper) => {
     if (!wrapper) return;
     wrapper.innerHTML = "";
     const editor = document.createElement("div");
     wrapper.append(editor);
-    new Quill(editor, { theme: "snow" });
+    new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR } });
   }, []);
 
   return <div id='text-editor-container' ref={editorRef}></div>;
