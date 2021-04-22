97753eb8589bcab9c70ebdfaf9ed4eda64e3cb50
Kaan Yilmaz <douscriptist@gmail.com>
2021-04-22 04:32:14 +0300

    socket server basic setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,20 @@
 import express, { Request, Response } from "express";
+import { Socket, Server, BroadcastOperator, Namespace, ServerOptions, RemoteSocket } from "socket.io";
 
 const app = express();
+const httpServer = require("http").createServer(app);
+const URL = "http://localhost:3000";
+const options: Partial<ServerOptions> = {
+  cors: {
+    origin: URL,
+    methods: ["GET", "POST"],
+  },
+};
+const io: Server = require("socket.io")(httpServer, options);
+
+io.on("connection", (socket: Socket) => {
+  console.log("hello");
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript online-docs Server!");
@@ -8,4 +22,4 @@ app.get("/", (req: Request, res: Response) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
+httpServer.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
