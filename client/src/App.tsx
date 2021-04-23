9eb911b373b382799b4f16de387ba4825e17cf44
Kaan Yilmaz <douscriptist@gmail.com>
2021-04-23 03:29:49 +0300

    router & uuid

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,27 @@
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { v4 } from "uuid";
 import EditorHeader from "components/EditorHeader";
 import TextEditor from "components/TextEditor";
+import MyDocuments from "components/MyDocuments";
+import Profile from "components/Profile";
 
 function App() {
   return (
     <div className='App'>
-      <EditorHeader />
-      <TextEditor />
+      <Router>
+        <Switch>
+          {/* TODO: refactor */}
+          <Route exact path='/'>
+            <Redirect to={`/document/${v4()}`} />
+          </Route>
+          <Route path='/my-documents' component={MyDocuments} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/document/:docId'>
+            <EditorHeader />
+            <TextEditor />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
