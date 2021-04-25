b05be651b78cf8a8be2f1193ae75da8453922a48
Kaan Yilmaz <douscriptist@gmail.com>
2021-04-25 08:57:09 +0300

    extend document context

--- client/src/context/DocumentContext.tsx
+++ client/src/context/DocumentContext.tsx
@@ -1,26 +1,42 @@
 import { createContext, FC, useContext, useState } from "react";
+import { Socket } from "socket.io-client";
+import Quill from "quill";
 import { Document } from "types";
 
 type NullableDoc = Omit<Document, "content"> | null;
 
 export interface DocumentContext {
-  document: NullableDoc;
+  document?: NullableDoc;
   setDocument: (doc: NullableDoc) => void;
+  socket?: Socket;
+  setSocket: (s: Socket) => void;
+  quill?: Quill;
+  setQuill: (q: Quill) => void;
 }
 
 const DocContext = createContext<DocumentContext>({
-  document: null,
+  document: undefined,
   setDocument: () => {},
+  socket: undefined,
+  setSocket: () => {},
+  quill: undefined,
+  setQuill: () => {},
 });
 
 export const useDocContext = () => useContext(DocContext);
 
 const DocContextProvider: FC = ({ children }) => {
-  const [document, setDocument] = useState<NullableDoc>(null);
+  const [document, setDocument] = useState<NullableDoc>();
+  const [socket, setSocket] = useState<Socket>();
+  const [quill, setQuill] = useState<Quill>();
 
   const value = {
     document,
     setDocument,
+    socket,
+    setSocket,
+    quill,
+    setQuill,
   };
 
   return <DocContext.Provider value={value}>{children}</DocContext.Provider>;
