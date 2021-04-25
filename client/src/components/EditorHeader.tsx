e61778f6fca791f74346fcd9d43917e9024f41e6
Kaan Yilmaz <douscriptist@gmail.com>
2021-04-25 08:48:18 +0300

    mini changes by doc context

--- client/src/components/EditorHeader.tsx
+++ client/src/components/EditorHeader.tsx
@@ -1,21 +1,37 @@
-import React from "react";
+import { formatDistance, formatDistanceToNow, subDays } from "date-fns";
+import { useDocContext } from "context/DocumentContext";
+import { useHistory } from "react-router";
 
 interface Props {}
 
 // TODO: refactor this component
 const EditorHeader = (props: Props) => {
+  const { document } = useDocContext();
+  const history = useHistory();
   return (
     <div id='editor-header'>
       <div>
+        <div>
+          <button onClick={() => history.push("/profile")}>profile</button>
+          <button onClick={() => history.push("/")}>reset</button>
+        </div>
         <div>ICon</div>
       </div>
       <div style={{ textAlign: "center" }}>
-        <div>Adsiz dosya</div>
-        <div>Son guncelleme: 15 dakika once</div>
+        <div>{document?.title}</div>
+        <div>Son guncelleme: {new Date(document?.updatedAt as string).toLocaleString("tr")}</div>
+        <div>
+          Son guncelleme:{" "}
+          {document &&
+            formatDistanceToNow(new Date(document?.updatedAt as string), {
+              addSuffix: true,
+              includeSeconds: true,
+            })}
+        </div>
       </div>
       <div>
-        <div>user</div>
-        <div>Paylas lock </div>
+        <div>user: {document?.owner}</div>
+        <div>Paylas/lock : {document?.privacy} </div>
       </div>
     </div>
   );
--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 interface Props {}
 
 const Profile = (props: Props) => {
+  const history = useHistory();
   return (
     <div>
       <h1>Profile</h1>
+      <button onClick={() => history.push("/document/75c6757b-48af-4d51-978f-27e2c409da12")}>go to document</button>
     </div>
   );
 };
