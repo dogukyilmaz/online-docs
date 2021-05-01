546154a27b8017ad70ec2d80f82cbc35224032b0
Kaan Yilmaz <douscriptist@gmail.com>
2021-05-01 05:51:32 +0300

    fix empty/loading document save issue

--- client/src/hooks/useAutoSaver.tsx
+++ client/src/hooks/useAutoSaver.tsx
@@ -3,16 +3,17 @@ import { useDocContext } from "context/DocumentContext";
 import { Events } from "types";
 
 const useAutoSaver = (intervalMs = 10000) => {
-  const { quill, socket } = useDocContext();
+  const { quill, socket, document } = useDocContext();
 
   useEffect(() => {
+    if (!document) return;
     const interval = setInterval(() => {
       socket?.emit(Events.SAVE_DOCUMENT, quill?.getContents());
     }, intervalMs);
 
     return () => clearInterval(interval);
     // eslint-disable-next-line
-  }, [quill, socket]);
+  }, [document, quill, socket]);
 };
 
 export default useAutoSaver;
