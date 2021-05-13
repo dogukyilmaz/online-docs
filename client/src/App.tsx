71282ffa5c5ab8906e87274fd886dbf12888565f
Kaan Yilmaz <douscriptist@gmail.com>
2021-05-13 06:40:06 +0300

    context structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,40 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { v4 } from "uuid";
 import EditorHeader from "components/EditorHeader";
 import TextEditor from "components/TextEditor";
 import MyDocuments from "components/MyDocuments";
 import Profile from "components/Profile";
-
-import AuthContextProvider from "context/AuthContext";
-import DocContextProvider from "context/DocumentContext";
 import Login from "components/Login";
 import Register from "components/Register";
+import { useUserContext } from "context/UserContext";
 
 function App() {
-  return (
-    <AuthContextProvider>
-      <DocContextProvider>
-        <div className='App'>
-          <Router>
-            <Switch>
-              <Route path='/login' component={Login} />
-              <Route path='/register' component={Register} />
+  const { loadUser } = useUserContext();
 
-              {/* TODO: refactor */}
-              <Route exact path='/'>
-                <Redirect to={`/document/${v4()}`} />
-              </Route>
-              <Route path='/my-documents' component={MyDocuments} />
-              <Route path='/profile' component={Profile} />
-              <Route path='/document/:docId'>
-                <EditorHeader />
-                <TextEditor />
-              </Route>
-            </Switch>
-          </Router>
-        </div>
-      </DocContextProvider>
-    </AuthContextProvider>
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          {/* TODO: refactor */}
+          <Route exact path='/'>
+            <Redirect to={`/document/${v4()}`} />
+          </Route>
+          <Route path='/my-documents' component={MyDocuments} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/document/:docId'>
+            <EditorHeader />
+            <TextEditor />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import AuthContextProvider from "context/AuthContext";
+import DocContextProvider from "context/DocumentContext";
+import UserContextProvider from "context/UserContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <UserContextProvider>
+        <DocContextProvider>
+          <App />
+        </DocContextProvider>
+      </UserContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
